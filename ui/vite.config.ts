8dbbdcc9eb88edef235af4a18ca4ca3b62f93039
Leospace001 <yuen7895123@yahoo.com>
2026-02-21 15:56:14 +0800

    hot reload page

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: '**/*.svg',
-  build: {
-    assetsInlineLimit: 0,
-  },
+	plugins: [react()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true,
+		strictPort: true,
+		port: 5173,
+	},
 });
