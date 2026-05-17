d9ebb3c7e89e0b3d1c2842fd1c0753d065e59476
Leospace001 <yuen7895123@yahoo.com.hk>
2026-05-17 19:30:31 +0800

    update init sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,10 +13,17 @@ VALUES
     (
         1,
         '2026-05-17 06:46:31.781564',
-        'USER',
-        'USER',
+        'ROLE_ADMIN',
+        'ROLE_ADMIN',
         NULL
-    )ON CONFLICT (id) DO NOTHING;;
+    ),
+    (
+        2,
+        '2026-05-17 06:46:31.781564',
+        'ROLE_USER',
+        'ROLE_USER',
+        NULL
+    ) ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO
     users (
@@ -35,6 +42,25 @@ VALUES
         True,
         True,
         'leoadmin',
-        '$2a$10$3kxNOT4bNjb.qlujNjHiW.ZcfygUxTC5.Asmjrn8Ga6WvKktKOiMi',
+        '$2a$10$vmv94mzW3R9Ehr/5cGIddeIOZIUBKZzec44IpGeuydWtx6ahYHLMe',
         'admin'
     ) ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO
+    "user_role_assignments" (
+        "id",
+        "active",
+        "assigned_date",
+        "removed_date",
+        "role_id",
+        "user_id"
+    )
+VALUES
+    (
+        1,
+        '1',
+        '2026-05-17 09:37:35.24627',
+        '2026-05-17 09:37:35.246281',
+        1,
+        1
+    )
