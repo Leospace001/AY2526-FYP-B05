16b0013e36fffac108935f73e0c681d6fb836cd0
thomasyosh <thomas.yosh@tutanota.com>
2026-05-17 15:04:04 +0800

    update redirect to swagger

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -123,6 +123,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                            "/",
                             "/v3/api-docs/**",
                             "/swagger-ui/**", 
                             "/login",
