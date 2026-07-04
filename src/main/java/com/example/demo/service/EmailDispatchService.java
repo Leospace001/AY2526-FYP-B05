45f835cdeeb386d241db1ce13b6da5caa3a8519a
Leospace001 <yuen7895123@yahoo.com.hk>
2026-07-04 19:04:08 +0800

    update email schedular

--- src/main/java/com/example/demo/service/EmailDispatchService.java
+++ src/main/java/com/example/demo/service/EmailDispatchService.java
@@ -1,5 +1,12 @@
 package com.example.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.example.demo.config.AppTimeZone;
 import com.example.demo.dto.EmailMessageDto;
 import com.example.demo.model.EmailRecord;
